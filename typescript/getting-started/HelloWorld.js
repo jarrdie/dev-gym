58dc8a2e15bfe1b7948ab98b546bbc82d430aa88
José Pablo Arroyo <jarroyod@saludcastillayleon.es>
2018-08-08 15:55:18 +0200

    Hello world TypeScript

---
+++ typescript/getting-started/HelloWorld.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    return "Hello " + name;
+}
+function multiply(a, b) {
+    return a * b;
+}
+var user = "Pablo";
+console.log(greet(user));
+console.log(multiply(2, 3));
---
+++ typescript/getting-started/HelloWorld.ts
@@ -0,0 +1,12 @@
+function greet(name) {
+    return "Hello " + name;
+}
+
+function multiply(a: number, b: number) {
+    return a * b;
+}
+
+let user = "Pablo";
+
+console.log(greet(user));
+console.log(multiply(2, 3));
